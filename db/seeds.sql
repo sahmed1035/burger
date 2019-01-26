b24d5cd5b10463922632870f2e204733f154ea46
sahmed <sahmed@alumni.depaul.edu>
2019-01-26 14:17:03 -0600

    verifying jaws

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Cheese Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Dubble Cheese Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bleu Cheese Burger', false);
